81010a1aa6b662f93b0e931076ac4f2bbd8bef40
rameshseervi242628 <rameshseervi242628@gmail.com>
2025-06-01 11:58:04 +0530

    latest code

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import AllApps from './src/AllApps'
 import { NavigationContainer } from '@react-navigation/native'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import AllAppNavigations from './src/NavigationStacks/AllAppNavigations'
 
 export default function App() {
