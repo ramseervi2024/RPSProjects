a6f2d4723353e096d6c1dd9217dd34c5ae07f34a
Ramesh-Seervi <ramesh@imcrinox.com>
2026-01-12 21:25:38 +0530

    PokéSwipe RN Assignment - Final Submission

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     // <SafeAreaView style={{flex:1, backgroundColor:'#fff'}}>
       <NavigationContainer>
-        <AllAppNavigations />
+        <PokeSwipe />
       </NavigationContainer>
     // </SafeAreaView>
   )
