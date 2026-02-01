9ba1ffcb7755b9c7c8d1d5c82e5d4d2876ec0d3e
rameshseervi242628 <rameshseervi242628@gmail.com>
2026-02-01 22:35:50 +0530

    new chnages

--- src/Apps/HeritageVastra/src/navigation/StackNavigator.js
+++ src/Apps/HeritageVastra/src/navigation/StackNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import BottomTabs from './BottomTabs';
 import ProductDetailScreen from '../screens/ProductDetailScreen';
 import CartScreen from '../screens/CartScreen';
+import TraditionalWearScreen from '../screens/TraditionalWearScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const StackNavigator = () => {
             <Stack.Screen name="MainTabs" component={BottomTabs} />
             <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
             <Stack.Screen name="Cart" component={CartScreen} />
+            <Stack.Screen name="TraditionalWear" component={TraditionalWearScreen} />
         </Stack.Navigator>
     );
 };
